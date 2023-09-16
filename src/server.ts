b8e709f165886bedbb5dc41e51fdc06ea6e85542
RenanS4ntos <renan-bs@hotmail.com>
2023-09-16 09:46:39 -0300

    Update to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,20 @@
-import {fastify} from 'fastify';
-import { fastifyCors } from '@fastify/cors'
-import { getAllForwardingsRoute } from './routes/get-all-forwardings';
-import { getAllTeams } from './routes/get-all-teams';
-import { getAttendantsByTeam } from './routes/get-attendants-by-team';
+import 'dotenv/config'
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { getAllForwardingsRoute } from "./routes/get-all-forwardings";
+import { getAllTeams } from "./routes/get-all-teams";
+import { getAttendantsByTeam } from "./routes/get-attendants-by-team";
 
-const app = fastify()
+const app = fastify();
 
 app.register(fastifyCors, {
-  origin: '*',
-})
+  origin: "*",
+});
 
-app.register(getAllForwardingsRoute)
-app.register(getAllTeams)
-app.register(getAttendantsByTeam)
+app.register(getAllForwardingsRoute);
+app.register(getAllTeams);
+app.register(getAttendantsByTeam);
 
 app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+  port: Number(process.env.PORT)?? 3333,
+});
